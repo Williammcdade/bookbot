b5650ff22a5870492b12244a06243e3acb52a254
williammcdade <will.mcdade@protonmail.com>
2025-12-15 21:49:13 -0500

    updated, still working on sort

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_chars_dict
+from stats import get_num_words, get_chars_dict, get_sorted_chars_list
 
 
 def main():
@@ -6,8 +6,14 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
+    sorted_chars = get_sorted_chars_list(text)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(chars_dict)
+    print("--------- Character Count -------")
+    for char in sorted_chars:
+        print(str(char))
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -7,8 +7,18 @@ def get_chars_dict(text):
     chars = {}
     for c in text:
         lowered = c.lower()
-        if lowered in chars:
-            chars[lowered] += 1
-        else:
-            chars[lowered] = 1
+        if lowered.isalpha():
+            if lowered in chars:
+                chars[lowered] += 1
+                #chars[lowered].append()
+            else:
+                chars[lowered] = 1
     return chars
+
+def sort_on_value(item):
+    return item[1]
+
+def get_sorted_chars_list(text):
+    chars_dict = get_chars_dict(text)
+    sorted_chars = sorted(chars_dict.items(), key=sort_on_value, reverse=True)
+    return sorted_chars
