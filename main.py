fdaa6c117194f8994595c2b05675860f2dc95c7e
Pyscho <will.mcdade@protonmail.com>
2026-03-01 11:36:34 -0500

    going to rebase after this anyway

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 from stats import get_num_words, get_chars_dict, get_sorted_chars_list
+=======
+from stats import get_num_words, get_chars_dict, sorted_dict
+>>>>>>> 7fff7b9 (jic)
 
 
 def main():
@@ -6,6 +10,7 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
+<<<<<<< HEAD
     sorted_chars = get_sorted_chars_list(text)
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
@@ -15,6 +20,12 @@ def main():
     for char in sorted_chars:
         print(str(char))
 
+=======
+    sorted_dick = sorted_dict(chars_dict).sort
+    print(f"Found {num_words} total words")
+    print(chars_dict)
+    print(sorted_dick)
+>>>>>>> 7fff7b9 (jic)
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -7,6 +7,7 @@ def get_chars_dict(text):
     chars = {}
     for c in text:
         lowered = c.lower()
+<<<<<<< HEAD
         if lowered.isalpha():
             if lowered in chars:
                 chars[lowered] += 1
@@ -22,3 +23,17 @@ def get_sorted_chars_list(text):
     chars_dict = get_chars_dict(text)
     sorted_chars = sorted(chars_dict.items(), key=sort_on_value, reverse=True)
     return sorted_chars
+=======
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def sorted_dict(chars):
+    chars_list = []
+    for char in chars:
+        if char not in chars_list:
+            chars_list["char": char[0], "num": char[1]]
+    return chars_list        
+>>>>>>> 7fff7b9 (jic)
