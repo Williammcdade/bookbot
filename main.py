aca24a5625087718dca3e7502c7874368f85c1d6
Pyscho <will.mcdade@protonmail.com>
2026-03-29 23:49:30 -0400

    final

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from stats import get_num_words, get_chars_dict, get_sorted_chars_list
+import sys
+
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     sorted_chars = get_sorted_chars_list(text)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
