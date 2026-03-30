e292abd0884fb546fdd286613823ca9f4a69671a
Pyscho <will.mcdade@protonmail.com>
2026-03-29 23:20:20 -0400

    Worked!

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 from stats import get_num_words, get_chars_dict, get_sorted_chars_list
-=======
-from stats import get_num_words, get_chars_dict, sorted_dict
->>>>>>> 7fff7b9 (jic)
 
 
 def main():
@@ -10,7 +6,6 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-<<<<<<< HEAD
     sorted_chars = get_sorted_chars_list(text)
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
@@ -20,12 +15,6 @@ def main():
     for char in sorted_chars:
         print(str(char))
 
-=======
-    sorted_dick = sorted_dict(chars_dict).sort
-    print(f"Found {num_words} total words")
-    print(chars_dict)
-    print(sorted_dick)
->>>>>>> 7fff7b9 (jic)
 
 def get_book_text(path):
     with open(path) as f:
